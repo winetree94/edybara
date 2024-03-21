bfa64ad050b7004ea1a62ad0e7406aba9988e21c
winetree94 <winetree94@gmail.com>
2024-03-21 23:26:45 +0900

    ⚙️ docs - fix broken link

--- packages/docs/docusaurus.config.ts
+++ packages/docs/docusaurus.config.ts
@@ -137,7 +137,7 @@ const config: Config = {
           items: [
             {
               label: 'Tutorial',
-              to: '/docs/intro',
+              to: '/docs/introduction',
             },
           ],
         },
